743031e5cc0fb2fd00b0e02eafff5fbe68a377e2
igor guilherme <igorguilherme.s555@gmail.com>
2020-09-13 14:33:17 -0300

    Criado projeto maven para testes

---
+++ maven-github-action/src/main/java/com/igorgsousa/mavengithubaction/MavenGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.igorgsousa.mavengithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGithubActionApplication.class, args);
+	}
+
+}
---
+++ maven-github-action/src/test/java/com/igorgsousa/mavengithubaction/MavenGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorgsousa.mavengithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
